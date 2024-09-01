70e0e4a985ff830a96145f2e50c38cc0dd069240
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-09-01 20:21:13 +0700

    asset settings

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <>
-    <nav>
-        
-    </nav>
+      <nav></nav>
     </>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
+      <Hero />
+      <Gallery />
+      <Footer />
     </main>
   );
 }
