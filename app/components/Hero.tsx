aaa104e97943d9bc9204e7ddb534fbb69639947a
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-09-17 21:31:06 +0700

    add navbar & hero section (revision font h1)

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import Navbar from "./Navbar";
+import HeroImage from "/public/hero-pict.jpg";
 
 export default function Hero() {
   return (
     <>
-    <div></div>
+      <div
+        className="relative h-[800px] bg-cover bg-top px-[40px] text-white"
+        style={{ backgroundImage: `url(${HeroImage.src})` }}
+      >
+        <div className="relative max-w-[600px] pt-[400px]">
+          <h1 className="text-[44px] font-black" style={{ fontWeight: 2000 }}>
+            How To Say Happy Birthday Before I Go Back To Malang
+          </h1>
+
+          <p className="py-[20px]">
+            This is how Rakha express love. In the mean time you will understand
+            how my brain works. As you see this, Rakha want to say Happy
+            Birthday to his crush.
+          </p>
+          <p className="pb-[15px]">Click &apos;Play&apos; to see the details</p>
+          <div className="flex gap-x-[10px]">
+            <button className="py-2 px-5 flex bg-white rounded-[5px] text-black font-medium">
+              <svg
+                className="w-6 h-6 text-black"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              Play
+            </button>
+            <button className="py-2 px-5 flex bg-white/40 rounded-[5px] text-white font-medium">
+              <svg
+                className="w-6 h-6 text-white pr-[3px]"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              Info
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="top-0 fixed w-full">
+        <Navbar />
+      </div>
     </>
-  )
+  );
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,60 @@
 import React from "react";
 import Image from "next/image";
+import Logo from "/public/logo-netflix.png";
 
 export default function Navbar() {
   return (
     <>
-      <nav></nav>
+      <nav className="bg-gradient-to-b from-black/70 to-black/0 text-white">
+        <div className="flex justify-between px-[40px] py-[25px]">
+          {/* logo & nav */}
+          <div className="flex ">
+            <Image src={Logo} alt="logo-netplix" className="mr-[40px]" />
+            <ul className="flex gap-x-[15px] font-medium">
+              <li className="hover:text-slate-200 duration-200">Homepage</li>
+              <li className="hover:text-slate-200 duration-200">Series</li>
+              <li className="hover:text-slate-200 duration-200">Movies</li>
+              <li className="hover:text-slate-200 duration-200">
+                New and Popular
+              </li>
+              <li className="hover:text-slate-200 duration-200">My List</li>
+            </ul>
+          </div>
+          {/* search */}
+          <div className="flex gap-x-[15px]">
+            <svg
+              className="w-6 h-6 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <h5 className="uppercase text-[12px] flex items-end font-bold">
+              adult
+            </h5>
+            <svg
+              className="w-6 h-6 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z" />
+            </svg>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
